83e1923a4550b08a3cee8034e08968146ab1c75f
incrisz <incrisz4luv@gmail.com>
2025-02-28 01:04:54 +0100

    first

---
+++ terraform/7-ec2.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "ec2-worker1" {
+    ami = "ami-091f18e98bc129c4e"
+    instance_type = "t2.medium"
+    subnet_id = aws_subnet.public_subnet.id
+    vpc_security_group_ids = [ aws_security_group.sg_custom.id ]
+    key_name = "miseuwest2ipalacaprover"
+
+    user_data = <<-EOF
+        #!/bin/bash
+        sudo apt update -y
+        sudo apt install -y ansible git
+
+        # Clone the Ansible playbook from GitHub
+        git clone https://github.com/Incrisz/HNG-2025.git /home/ubuntu/HNG-2025
+
+        # Run the Ansible playbook
+        cd /home/ubuntu/HNG-2025/stage4/ansible
+        ansible-playbook playbook.yml
+    EOF
+
+    # Tag the instance as "ec2-worker1" to differentiate it from other instances.
+    tags = {
+        "Name" = "ec2-worker1"
+    }
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.ec2-worker1.public_ip
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,22 @@
+
+variable "aws-access_key" {
+  type = string
+  default = "dkhcvhchcghh"
+}
+variable "aws-secret_key" {
+  type = string
+  default = "hxgfyfvvuhvjg"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
